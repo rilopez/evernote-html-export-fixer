d4d70e3c453916396d03ca5f1615d45e835d9980
Isaac Lopez <rilsoft@gmail.com>
2021-06-05 11:40:49 -0500

    extract properties

--- bin/index.js
+++ bin/index.js
@@ -13,50 +13,81 @@ const options = yargs
         type: "string",
         demandOption: true
     })
+    .option("m", {
+        alias: "maxfiles",
+        describe: "Maximum number of files to process in directory, process all if zero",
+        type: "number",
+        default: 0
+
+    })
     .argv;
 
 console.log(`loading ${options.dir}`);
 
 
+function extractNoteProps($) {
+    const metaTags = $('.html-note meta');
+    let meta = {};
+    for (const metaTagsKey in metaTags) {
+        const tag = metaTags[metaTagsKey];
+        if (tag.type !== 'tag') continue;
+        meta[$(tag).attr('itemprop')] = $(tag).attr('content');
+    }
+    return meta;
+}
+
 fs.readdir(options.dir, (err, files) => {
-    const stats = { files: 0}
-    files.forEach(file => {
-        let resolvedPath = path.resolve(options.dir, file);
+    const stats = {files: 0}
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const resolvedPath = path.resolve(options.dir, file);
         const fileAttrs = fs.lstatSync(resolvedPath);
         if (!fileAttrs.isDirectory()) {
             console.log(`file: ${orange(resolvedPath)} `);
             const $ = cheerio.load(fs.readFileSync(resolvedPath));
-            const title = $('.html-note > h1').text()
+
+            const noteProps = extractNoteProps($);
+            const $h1 = $('.html-note > h1');
+            $(`<pre>${JSON.stringify(noteProps,null,4)}</pre>`).insertAfter($h1);
+            const title = $h1.text()
             console.log(`\t title: ${title}`);
-            //TODO extract note attrs
-            /*<div class="html-note">
-                  <meta itemprop="title" content="PDF - www.dgb.sep.gob.mx">
-                  <meta itemprop="created" content="20200207T232114Z">
-                  <meta itemprop="updated" content="20200207T232114Z">
-                  <note-attributes>
-                    <meta itemprop="source" content="web.clip7">
-                    <meta itemprop="source-url" content="https://www.dgb.sep.gob.mx/acciones-y-programas/PDF/GUIA%20DE%20ESTUDIOS%202018-2019.pdf">
-                    <meta itemprop="source-application" content="webclipper.evernote">
-                  </note-attributes>
-             */
+
+            //TODO replace meta tags with PRE tag with props in json format
             //TODO verify if it has a `${resolvedPath} files` folder
             //TODO fix href absolute paths
             //TODO href paths verify if it exists
             //TODO if found a PDF . add link, move the file, same folder
-            stats.files = stats.files + 1;
 
-            //TODO call pandoc `pandoc  PDF\ -\ www.dgb.sep.gob.mx.html   -o /tmp/test2.pdf`
-            //TODO add readme pandoc / latex installation
-            /*
-              sudo apt install pandoc
-              sudo apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
-             */
 
-            //TODO set generated pdf file attrs to match  note attrs
+
+            fs.writeFile(resolvedPath, $.html(), err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                console.log(`file  ${resolvedPath} fixed successfully` )
+                //TODO call pandoc `pandoc  PDF\ -\ www.dgb.sep.gob.mx.html   -o /tmp/test2.pdf`
+                //TODO add readme pandoc / latex installation
+                /*
+                  sudo apt install pandoc
+                  sudo apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
+                 */
+
+                //TODO set generated pdf file attrs to match  note attrs
+            });
+
+
+
             //TODO move all generated PDF files to a folder
 
+            stats.files = stats.files + 1;
+            if ( options.maxfiles !== 0 && options.maxfiles <= stats.files ) {
+                break
+            }
         }
-    });
+    }
+
     console.log(stats);
 });
 
