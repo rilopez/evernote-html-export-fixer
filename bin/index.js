4abfaad9a2eeff9a0cc12445987bcbed5fc2f6c7
Isaac Lopez <rilsoft@gmail.com>
2021-06-07 00:03:15 -0500

    feature complete

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+const quote = require('shell-quote').quote;
+
 const cheerio = require('cheerio');
 const {execSync} = require('child_process');
 const path = require('path');
@@ -27,6 +29,13 @@ const options = yargs
         describe: "abs directory prefix,  usually is the destiation path used during evernote export",
         type: "string",
         default: "Z:"
+
+    })
+    .option("e", {
+        alias: "pdfengines",
+        describe: "comma separated pandoc pdf engines",
+        type: "string",
+        default: "weasyprint,pdflatex"
 
     })
     .argv;
@@ -125,61 +134,91 @@ function execCommand(cmd, cwd) {
     return stdout;
 }
 
+function getPdfFileName(htmlFilePathAttrs) {
+    return path.join(htmlFilePathAttrs.dir, htmlFilePathAttrs.name + '.pdf');
+}
+
 /**
  * Converts a HTML file to PDF using panda
  * @param htmlFilePathAttrs
  * @param htmlfileFullPath
  */
 function createPDF(title, htmlFilePathAttrs, htmlfileFullPath, noteProps) {
-
-    const pdfFile = path.join(htmlFilePathAttrs.dir, htmlFilePathAttrs.name + '.pdf');
-
-    execCommand('pandoc', "${htmlfileFullPath}"  -o "${pdfFile}" --pdf-engine=weasyprint  --metadata title="${title}"`, htmlFilePathAttrs.dir);
-
-    const createdFormated = DateTime.fromISO(noteProps.created).toFormat('yyyyMMddhhmm.ss');
-    execCommand(`touch  -a -m -t ${createdFormated} "${pdfFile}"`, htmlFilePathAttrs.dir);
-
-    const keywords = noteProps.tags ? `-Keywords="${noteProps.tags.join(',')}"` : '';
-    execCommand(`exiftool -overwrite_original_in_place -CreateDate=${noteProps.created} -ModifyDate=${noteProps.modified}  ${keywords} "${pdfFile}"`, htmlFilePathAttrs.dir);
+    const keywords = noteProps.tags ? `-Keywords=${quote([noteProps.tags.join(',')])}` : '';
+    const pdfFile = getPdfFileName(htmlFilePathAttrs);
+    const pandocCmd = `pandoc ${quote([htmlfileFullPath])}  -o ${quote([pdfFile])}  --metadata title=${quote([title])}`;
+    const engines= options.pdfengines.split(',');
+    for (const engine of engines) {
+        try {
+            execCommand(`${pandocCmd} --pdf-engine=${engine}`, htmlFilePathAttrs.dir);
+            //const createdFormatted = DateTime.fromISO(noteProps.created).toFormat('yyyyMMddhhmm.ss');
+            //execCommand(`touch  -a -m -t ${createdFormatted} ${quote([pdfFile])}`, htmlFilePathAttrs.dir);
+            execCommand(`exiftool -overwrite_original_in_place -CreateDate=${noteProps.created} -ModifyDate=${noteProps.updated}  ${keywords} ${quote([pdfFile])}`, htmlFilePathAttrs.dir);
+            break
+        } catch (e) {
+            console.error(`not able to generate pdf file using ${engine}: ${chalk.bold(pdfFile)}`, e);
+        }
+    }
 }
 
 function writeFile(title, absFilePath, html, noteProps) {
-    const filePathAttrs = path.parse(absFilePath)
+    const filePathAttrs = path.parse(absFilePath);
     fs.writeFileSync(absFilePath, html);
     createPDF(title, filePathAttrs, absFilePath, noteProps);
     console.log(`file  ${absFilePath} fixed successfully`)
 }
 
-fs.readdir(options.dir, (err, files) => {
-    const stats = {files: 0}
+async function processFile(statusStr, absFilePath, filePathAttrs) {
+    console.log(`${statusStr} file: ${orange(absFilePath)} `);
+    const $ = cheerio.load(fs.readFileSync(absFilePath));
+    const noteProps = extractNoteProps($);
+
+    //TODO do not modify html if tag pre.ril-note-props-json is found
+    const $h1 = $('.html-note > h1');
+
+    $('.html-note').append($(`<pre class="ril-note-props-json">${JSON.stringify(noteProps, null, 4)}</pre>`));
+    const title = $h1.text().trim()
+    const filesFolder = getFilesFolderName(filePathAttrs);
+    console.log(`
+               title: ${title}
+               filesFolder: ${filesFolder}
+            `);
+    fixImages($);
+    fixFileReferences($, 'a', 'href');
+    fixPDFViewer($, filesFolder);
+    writeFile(title, absFilePath, $.html(), noteProps);
+    return `${statusStr} COMPLETED`;
+}
+
+fs.readdir(options.dir, (err, allFiles) => {
+
+    const files = allFiles.filter(f => {
+        const absFilePath = path.resolve(options.dir, f);
+        const filePathAttrs = path.parse(absFilePath);
+        const fileAttrs = fs.lstatSync(absFilePath);
+        const pdfFile = getPdfFileName(filePathAttrs);
+        const isHTML = filePathAttrs.ext === ".html";
+        const pdfNotFound = !fs.existsSync(pdfFile);
+        return (fileAttrs.isFile()
+            && filePathAttrs.name !== 'Evernote_index'
+            && isHTML
+            && pdfNotFound
+        );
+    });
+
+    const stats = {totalFiles: files.length, filesFixed: 0};
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         const absFilePath = path.resolve(options.dir, file);
         const filePathAttrs = path.parse(absFilePath)
-        const fileAttrs = fs.lstatSync(absFilePath);
-        if (!fileAttrs.isDirectory() || filePathAttrs.ext === ".html") {
-            console.log(`file: ${orange(absFilePath)} `);
-            const $ = cheerio.load(fs.readFileSync(absFilePath));
-            const noteProps = extractNoteProps($);
-            const $h1 = $('.html-note > h1');
-            $(`<pre>${JSON.stringify(noteProps, null, 4)}</pre>`).insertAfter($h1);
-            const title = $h1.text().trim()
-            const filesFolder = getFilesFolderName(filePathAttrs);
-            console.log(`
-               title: ${title}
-               filesFolder: ${filesFolder}
-            `);
-
-            fixImages($);
-            fixFileReferences($, 'a', 'href');
-            fixPDFViewer($, filesFolder);
-            writeFile(title, absFilePath, $.html(), noteProps);
-            stats.files = stats.files + 1;
-            if (options.maxfiles !== 0 && options.maxfiles <= stats.files) {
-                break
-            }
+        stats.filesFixed = stats.filesFixed + 1;
+        const statusStr = chalk.bold(`[${stats.filesFixed} of ${stats.totalFiles}]`);
+        processFile(statusStr, absFilePath, filePathAttrs).then(r => console.log(r));
+        if (options.maxfiles !== 0 && options.maxfiles <= stats.filesFixed) {
+            break
         }
+
     }
 
     console.log(stats);
